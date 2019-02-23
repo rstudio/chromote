50f6f54dfb6e4e892f4268cf46b92af611fe2443
Winston Chang <winston@stdout.org>
2019-02-22 21:45:54 -0600

    Add hybrid_chain

--- R/promises.R
+++ R/promises.R
@@ -22,3 +22,53 @@ promise_timeout <- function(p, timeout, loop = current_loop(),
     )
   })
 }
+
+
+
+# More convenient way of chaining together promises than then/catch/finally,
+# without the performance impact of %...>%.
+promise_chain <- function(p, ..., catch = NULL, finally = NULL) {
+  for (fn in list(...)) {
+    p <- p$then(fn)
+  }
+
+  if (!is.null(catch)) {
+    p <- p$catch(catch)
+  }
+
+  if (!is.null(finally)) {
+    p <- p$finally(finally)
+  }
+
+  p
+}
+
+
+hybrid_chain <- function(expr, ..., catch = NULL, finally = NULL) {
+  runFinally <- TRUE
+  tryCatch(
+    {
+      result <- force(expr)
+
+      if (promises::is.promising(result)) {
+        p <- promise_chain(result, ..., catch = catch, finally = finally)
+        runFinally <- FALSE
+        invisible(p)
+
+      } else {
+        for (fn in list(...)) {
+          result <- fn(result)
+        }
+      }
+    },
+    error = function(e) {
+      if (!is.null(catch))
+        catch(e)
+      else
+        stop(e)
+    },
+    finally = {
+      if (runFinally && !is.null(finally)) finally()
+    }
+  )
+}
