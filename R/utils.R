a75b3bd828e87db02832b57b3ff0cd65bfe852b9
Garrick Aden-Buie <garrick@adenbuie.com>
2023-03-20 15:00:51 -0400

    Use a random port when connecting to headless chrome

--- R/utils.R
+++ R/utils.R
@@ -92,3 +92,122 @@ browse_url <- function(path, chromote) {
     utils::browseURL(url)
   }
 }
+
+
+# =============================================================================
+# Random Ports
+# =============================================================================
+#
+# Borrowed from https://github.com/rstudio/httpuv/blob/main/R/random_port.R
+
+with_random_port <- function(
+  .f,
+  min = 1024L,
+  max = 49151L,
+  n = 5,
+  host = "http://127.0.0.1"
+) {
+  valid_ports <- setdiff(seq.int(min, max), unsafe_ports)
+
+  # Try up to n ports
+  n <- min(n, length(valid_ports))
+  ports <- sample(valid_ports, n)
+  for (port in ports) {
+    res <- NULL
+
+    # Try to run `.f()` with the random port
+    res <- tryCatch(
+      .f(host = host, port = port),
+      error = function(err) if (identical(port, ports[n])) err,
+      error_timeout = identity,
+      system_command_error = identity
+    )
+
+    if (rlang::cnd_inherits(res, "error")) {
+      # A timeout error or trying all ports is unlikely to be port-related problem
+      rlang::cnd_signal(res)
+    }
+
+    if (!is.null(res)) {
+      return(res)
+    }
+  }
+
+  rlang::abort(
+    "Cannot find an available port. Please try again.",
+    class = "chromote_error_no_available_port"
+  )
+}
+
+# Ports that are considered unsafe by Chrome
+# http://superuser.com/questions/188058/which-ports-are-considered-unsafe-on-chrome
+# https://github.com/rstudio/shiny/issues/1784
+unsafe_ports <- c(
+  1,
+  7,
+  9,
+  11,
+  13,
+  15,
+  17,
+  19,
+  20,
+  21,
+  22,
+  23,
+  25,
+  37,
+  42,
+  43,
+  53,
+  77,
+  79,
+  87,
+  95,
+  101,
+  102,
+  103,
+  104,
+  109,
+  110,
+  111,
+  113,
+  115,
+  117,
+  119,
+  123,
+  135,
+  139,
+  143,
+  179,
+  389,
+  427,
+  465,
+  512,
+  513,
+  514,
+  515,
+  526,
+  530,
+  531,
+  532,
+  540,
+  548,
+  556,
+  563,
+  587,
+  601,
+  636,
+  993,
+  995,
+  2049,
+  3659,
+  4045,
+  6000,
+  6665,
+  6666,
+  6667,
+  6668,
+  6669,
+  6697
+)
