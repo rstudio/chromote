75cc2264c844468dfeb80548b84336e9f800b3db
Garrick Aden-Buie <garrick@adenbuie.com>
2023-08-10 16:26:30 -0400

    Upkeep for v0.1.2 (#116)
    * chore: RStudio is now Posit
    
    * chore: Add pkgdown link to DESCRIPTION
    
    * chore: use_tidy_description()
    
    * chore: use package doc and consolidate importFrom there
    
    * chore: Replace event loop explanation video
    
    The previous link no longer works and the blog no longer exists. I picked a talk at JSConf from 2014 that does a great job visually explaining the JS event loop. Hope this works!

---
+++ R/chromote-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import promises later
+#' @importFrom fastmap fastmap
+#' @importFrom jsonlite fromJSON toJSON
+#' @importFrom processx process
+#' @importFrom R6 R6Class
+#' @importFrom websocket WebSocket
+## usethis namespace: end
+NULL
