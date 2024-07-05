d11b031c8e78633f99cddda0f78ec2886743a099
José Francisco Loff <jfloff@phistat.com>
2024-07-05 23:12:15 +0100

    Add support for OpenBSD
    Add a test for OpenBSD.  Also, Chromium's package installs the
    executable as `chrome`, so add it to the search list.

--- R/chrome.R
+++ R/chrome.R
@@ -87,7 +87,14 @@ find_chrome <- function() {
     } else if (is_linux()) {
       inform_if_chrome_not_found(
         find_chrome_linux(),
-        searched_for = "`google-chrome` and `chromium-browser` were",
+        searched_for = "`google-chrome`, `chromium-browser` and `chrome` were",
+        extra_advice = "or adding one of these executables to your PATH"
+      )
+
+    } else if (is_openbsd()) {
+      inform_if_chrome_not_found(
+        find_chrome_linux(),
+        searched_for = "`google-chrome`, `chromium-browser` and `chrome` were",
         extra_advice = "or adding one of these executables to your PATH"
       )
 
@@ -127,7 +134,8 @@ find_chrome_linux <- function() {
     "chromium-browser",
     "chromium",
     "google-chrome-beta",
-    "google-chrome-unstable"
+    "google-chrome-unstable",
+    "chrome"
   )
 
   for (path in possible_names) {
