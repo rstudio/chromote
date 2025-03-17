c5d15d5e10557a82ccc015fc7a1c7fd45c1bf87c
Garrick Aden-Buie <garrick@adenbuie.com>
2025-03-17 10:57:21 -0400

    tests: Get latest-stable chrome if not on CRAN and no envvar set

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(chromote)
 
 on_cran <- !interactive() && isTRUE(as.logical(Sys.getenv("NOT_CRAN", "false")))
 
-if (!on_cran && !identical(Sys.getenv("CHROMOTE_CHROME"), "")) {
+if (!on_cran && identical(Sys.getenv("CHROMOTE_CHROME"), "")) {
   path <- chrome_versions_add("latest-stable", "chrome")
   Sys.setenv("CHROMOTE_CHROME" = path)
 }
