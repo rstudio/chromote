f5215a7a90d9e770c83bb43a25f90503f7f4a513
Garrick Aden-Buie <garrick@adenbuie.com>
2025-03-18 11:29:58 -0400

    fix: can't skip in setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,9 @@
-skip_on_cran()
+on_cran <- !isTRUE(as.logical(Sys.getenv("NOT_CRAN", "false")))
 
-has_chromote_envvar <- !identical(Sys.getenv("CHROMOTE_CHROME"), "")
+if (!on_cran) {
+  has_chromote_envvar <- !identical(Sys.getenv("CHROMOTE_CHROME"), "")
 
-if (!has_chromote_envvar) {
-  local_chrome_version("latest-stable", "chrome")
+  if (!has_chromote_envvar) {
+    local_chrome_version("latest-stable", "chrome")
+  }
 }
